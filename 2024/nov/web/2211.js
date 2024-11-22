ec08035eb23d7747c860b0197ed1a10791e4b535
Ricardo A Souza <cursowebmx@gmail.com>
2024-11-22 15:29:17 -0400

    22nov2024
    producao demonstração

---
+++ 2024/nov/web/2211.js
@@ -0,0 +1,51 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "2211_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 90,
+	        "minpan": 0,
+	        "mintilt": -90
+	    },
+	"id": "2211",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"2211_tiles/2211_t_1b_%y_%x.jpg"},"down":{"src":"2211_tiles/2211_t_1d_%y_%x.jpg"},"front":{"src":"2211_tiles/2211_t_1f_%y_%x.jpg"},"height":3453,"left":{"src":"2211_tiles/2211_t_1l_%y_%x.jpg"},"right":{"src":"2211_tiles/2211_t_1r_%y_%x.jpg"},"tilesize":480,"up":{"src":"2211_tiles/2211_t_1u_%y_%x.jpg"},"width":3453},{"back":{"src":"2211_tiles/2211_t_2b_%y_%x.jpg"},"down":{"src":"2211_tiles/2211_t_2d_%y_%x.jpg"},"front":{"src":"2211_tiles/2211_t_2f_%y_%x.jpg"},"height":1727,"left":{"src":"2211_tiles/2211_t_2l_%y_%x.jpg"},"right":{"src":"2211_tiles/2211_t_2r_%y_%x.jpg"},"tilesize":480,"up":{"src":"2211_tiles/2211_t_2u_%y_%x.jpg"},"width":1727},{"back":{"src":"2211_tiles/2211_t_3b_%y_%x.jpg"},"down":{"src":"2211_tiles/2211_t_3d_%y_%x.jpg"},"front":{"src":"2211_tiles/2211_t_3f_%y_%x.jpg"},"height":864,"left":{"src":"2211_tiles/2211_t_3l_%y_%x.jpg"},"right":{"src":"2211_tiles/2211_t_3r_%y_%x.jpg"},"tilesize":480,"up":{"src":"2211_tiles/2211_t_3u_%y_%x.jpg"},"width":864},{"back":{"src":"2211_tiles/2211_t_4b_%y_%x.jpg"},"down":{"src":"2211_tiles/2211_t_4d_%y_%x.jpg"},"front":{"src":"2211_tiles/2211_t_4f_%y_%x.jpg"},"height":432,"left":{"src":"2211_tiles/2211_t_4l_%y_%x.jpg"},"right":{"src":"2211_tiles/2211_t_4r_%y_%x.jpg"},"tilesize":480,"up":{"src":"2211_tiles/2211_t_4u_%y_%x.jpg"},"width":432}],"multilevel":true,"preview":{"src":"2211_tiles/2211_preview.jpg"},"projection":"cubic"},
+	"textbox": [
+	        {
+	            "align": "top",
+	            "style": " color:#ffffff; font-family: Arial,Helvetica,sans-serif; text-shadow: 0px 0px 3.8px #000000, 0px 0px 2.5px #000000, 0px 0px 1.7px #000000; font-size: 12px; font-weight: bold; text-align:center;",
+	            "text": "22nov",
+	            "yoff": "4px"
+	        }
+	    ],
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
---
+++ 2024/nov/web/2211c2.js
@@ -0,0 +1,43 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "2211c2_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 90,
+	        "minpan": 0,
+	        "mintilt": -90
+	    },
+	"id": "2211c2",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"2211c2_tiles/2211c2_t_1b_%y_%x.jpg"},"down":{"src":"2211c2_tiles/2211c2_t_1d_%y_%x.jpg"},"front":{"src":"2211c2_tiles/2211c2_t_1f_%y_%x.jpg"},"height":3491,"left":{"src":"2211c2_tiles/2211c2_t_1l_%y_%x.jpg"},"right":{"src":"2211c2_tiles/2211c2_t_1r_%y_%x.jpg"},"tilesize":480,"up":{"src":"2211c2_tiles/2211c2_t_1u_%y_%x.jpg"},"width":3491},{"back":{"src":"2211c2_tiles/2211c2_t_2b_%y_%x.jpg"},"down":{"src":"2211c2_tiles/2211c2_t_2d_%y_%x.jpg"},"front":{"src":"2211c2_tiles/2211c2_t_2f_%y_%x.jpg"},"height":1746,"left":{"src":"2211c2_tiles/2211c2_t_2l_%y_%x.jpg"},"right":{"src":"2211c2_tiles/2211c2_t_2r_%y_%x.jpg"},"tilesize":480,"up":{"src":"2211c2_tiles/2211c2_t_2u_%y_%x.jpg"},"width":1746},{"back":{"src":"2211c2_tiles/2211c2_t_3b_%y_%x.jpg"},"down":{"src":"2211c2_tiles/2211c2_t_3d_%y_%x.jpg"},"front":{"src":"2211c2_tiles/2211c2_t_3f_%y_%x.jpg"},"height":873,"left":{"src":"2211c2_tiles/2211c2_t_3l_%y_%x.jpg"},"right":{"src":"2211c2_tiles/2211c2_t_3r_%y_%x.jpg"},"tilesize":480,"up":{"src":"2211c2_tiles/2211c2_t_3u_%y_%x.jpg"},"width":873},{"back":{"src":"2211c2_tiles/2211c2_t_4b_%y_%x.jpg"},"down":{"src":"2211c2_tiles/2211c2_t_4d_%y_%x.jpg"},"front":{"src":"2211c2_tiles/2211c2_t_4f_%y_%x.jpg"},"height":437,"left":{"src":"2211c2_tiles/2211c2_t_4l_%y_%x.jpg"},"right":{"src":"2211c2_tiles/2211c2_t_4r_%y_%x.jpg"},"tilesize":480,"up":{"src":"2211c2_tiles/2211c2_t_4u_%y_%x.jpg"},"width":437}],"multilevel":true,"preview":{"src":"2211c2_tiles/2211c2_preview.jpg"},"projection":"cubic"},
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
